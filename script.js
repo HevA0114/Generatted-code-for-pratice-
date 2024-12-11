4de7f2ea59d8a1f697f83236daf9b64a6988a9f1
HevA0114 <albaytarheaven@gmail.com>
2024-12-11 17:29:02 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add smooth scrolling to navigation links
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            e.preventDefault();
+            target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
